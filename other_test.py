e2bf8446c1ec320d126a279db958eda2ae4d0f1c
gabrodval <lotorrpg@gmail.com>
2026-07-13 14:20:00 -0300

    clear

--- other_test.py
+++
@@ -1,9 +0,0 @@
-
-
-bigassarray = open('bigassarray.txt', 'a')
-bigassarray.write('[')
-for it in range(152, 387):
-    bigassarray.write(f'[{str(it)}],')
-bigassarray.write(']')
-
-bigassarray.close
